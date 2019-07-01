2f3d1e2626f5326d093d10be7437412146d63485
Du Phan <dphan@MacBook-Pro-de-Dataiku-3.local>
2019-07-01 18:07:00 +0200

    add Tooltips

--- resource/treemap-chart.js
+++ resource/treemap-chart.js
@@ -1,24 +1,28 @@
 let dataReady;
 let chartReady;
 
-function initWaterfall(cfg, onReady) {
+
+
+function initTreemap(cfg, onReady) {
     try {
         dataiku.checkWebAppParameters();
     } catch (e) {
         webappMessages.displayFatalError(e.message + ' Go to settings tab.');
         return;
     }
-    
+
     console.warn('CFG ', cfg)
     const dataset = cfg['dataset'];
     const unit = cfg['unit'];
-    const value = cfg['value'];
-    console.warn('----VALUE: ',value)
+    const parent = cfg['parent'];
+    const cell_size = cfg['cell_size'];
+    const color = cfg['color'];
+    console.warn('----COLOR: ', color)
     const sampling = {};
 
     let allRows;
     let old_record = null;
-    
+
     function transform_record(record){
         var row = [];
         row.push(record[unit]);
@@ -54,10 +58,24 @@ function initWaterfall(cfg, onReady) {
             drawAppIfEverythingReady();
         });
         dataiku.fetch(dataset, sampling, function(dataFrame) {
-            allRows = dataFrame.mapRecords(transform_record);
-            var last_index = allRows.length - 1;
-            var final_row = ["final", "0", "0", allRows[last_index][4], allRows[last_index][4]];
-            allRows.push(final_row);
+            //allRows = dataFrame.mapRecords(transform_record);
+            //var last_index = allRows.length - 1;
+            //var final_row = ["final", "0", "0", allRows[last_index][4], allRows[last_index][4]];
+            //allRows.push(final_row);
+
+            if (typeof color == 'undefined'){
+                console.warn('3 COLUMNS ONLY !!!')
+                var columnNames = [[unit, parent, cell_size]];
+                var rows = dataFrame.mapRecords(r => [r[unit], r[parent], parseFloat(r[cell_size])]);
+            }
+            else{
+                console.warn('4 COLUMNS !!!')
+                var columnNames = [[unit, parent, cell_size, color]];
+                var rows = dataFrame.mapRecords(r => [r[unit], r[parent], parseFloat(r[cell_size]), parseFloat(r[color])]);
+            }
+            //console.warn('columnNames', typeof columnNames[0])
+            //console.warn('rows', typeof rows[0])
+            allRows = columnNames.concat(rows);
             dataReady = true;
             drawAppIfEverythingReady();
         });
--- webapps/treemap-chart/app.js
+++ webapps/treemap-chart/app.js
@@ -4,34 +4,61 @@ let webAppConfig = dataiku.getWebAppConfig()['webAppConfig'];
 function draw() {
 
     console.warn('DRAWING ', allRows);
+
+    /*
     let data = new google.visualization.DataTable();
-    data.addColumn('string', 'label');
-    data.addColumn('number', 'min_threshold');
-    data.addColumn('number', 'min_value');
-    data.addColumn('number', 'max_value');
-    data.addColumn('number', 'max_threshold');
-    
+    data.addColumn('string', 'unit');
+    data.addColumn('string', 'parent');
+    data.addColumn('number', 'size');
+    data.addColumn('number', 'color');
+
     for (var i = 0; i < allRows.length; i++) {
         arr = allRows[i];
-        data.addRow([arr[0], parseInt(arr[1]), parseInt(arr[2]), parseInt(arr[3]), parseInt(arr[4])]);
-    }
+        data.addRow([arr[0], arr[1], parseFloat(arr[2]), parseFloat(arr[3])]);
+    }*/
 
-    let options = {
-      legend: 'none',
-      bar: { groupWidth: '100%' }, // Remove space between bars.
-      candlestick: {
-        fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
-        risingColor: { strokeWidth: 0, fill: '#0f9d58' }   // green
+    let data = new google.visualization.arrayToDataTable(allRows)
+    function showFullTooltip(row, size, value) {
+        if (data.getNumberOfColumns() == 4){
+            return '<div style="background:#fd9; padding:10px; border-style:solid">' +
+                   '<span style="font-family:Courier"><b>' + data.getValue(row, 0) +
+                   '</b>, ' + data.getValue(row, 1) + '</span><br>' +
+               data.getColumnLabel(2) +
+                   ' (total size value of this cell and its children): ' + size + '<br>' +
+               data.getColumnLabel(3) + ' (color value): ' + data.getValue(row, 2) + ' </div>';
+        }
+        else{
+            return '<div style="background:#fd9; padding:10px; border-style:solid">' +
+                   '<span style="font-family:Courier"><b>' + data.getValue(row, 0) +
+                   '</b>, ' + data.getValue(row, 1) + '</span><br>' +
+               data.getColumnLabel(2) + ' (total value of this cell and its children): ' + size + '<br>' +
+                ' </div>';
+        }
       }
-    };
-
-    let chart = new google.visualization.CandlestickChart(document.getElementById('waterfall-chart'));
+    let  options = {
+        highlightOnMouseOver: true,
+        maxDepth: 1,
+        maxPostDepth: 2,
+        minHighlightColor: '#8c6bb1',
+        midHighlightColor: '#9ebcda',
+        maxHighlightColor: '#edf8fb',
+        minColor: '#009688',
+        midColor: '#f7f7f7',
+        maxColor: '#ee8100',
+        headerHeight: 15,
+        showScale: true,
+        showTooltips: true,
+        height: 500,
+        useWeightedAverageForAggregation: true,
+        generateTooltip: showFullTooltip
+      };
+    let chart = new google.visualization.TreeMap(document.getElementById('treemap-chart'));
     chart.draw(data, options);
 }
-    
-initWaterfall( webAppConfig, (data) => {
+
+initTreemap( webAppConfig, (data) => {
     allRows = data;
-    draw(); 
+    draw();
 });
 
 window.addEventListener('message', function(event) {
@@ -40,7 +67,7 @@ window.addEventListener('message', function(event) {
         if (!allRows) {
             return;
         }
-        initWaterfall(webAppConfig, (data) => {
+        initTreemap(webAppConfig, (data) => {
             allRows = data;
             draw();
         });;
